7ee1aa75b5e05945ed2cfd4992f1ab94f4798364
Andrew McLean <88289750+itsreallydrew@users.noreply.github.com>
2021-12-08 01:03:06 +0000

    character and boss UI render finished

--- src/Components/Arena/Arena.jsx
+++ src/Components/Arena/Arena.jsx
@@ -34,11 +34,57 @@ const Arena = ({characterNFT}) => {
     }
   }, [gameContract])
 
+  const runAttack = async () => {
+
+  }
+
   return (
     <div className='arena-container'>
-      <p>Boss</p>
+      {boss && (
+        <div className='boss-container'>
+          <div className={`boss-content`}>
+            <h2>🐲  {boss.name} 🐲</h2>
+            <div className='image-content'>
+            <img src={boss.imageURI} alt={`Boss ${boss.name}`}/>
+            <div className='health-bar'>
+              <progress value={boss.hp} max={boss.maxHp} />
+              <p>{`${boss.hp} / ${boss.maxHp} HP`}</p>
+            </div>
+          </div>
+        </div>
+        <div className='attack-container'>
+          <button className='cta-button' onClick={runAttack}>
+            {`⚔️ Attack ${boss.name}`}
+          </button>
+        </div>
+      </div>          
+      )
+      }
+      {characterNFT && (
+        <div className='players-container'>
+          <div className='player-container'>
+            <h2>Your Character</h2>
+              <div className='player'>
+                <div className='image-content'>
+                  <h2>{characterNFT.name}</h2>
+                  <img 
+                  src={characterNFT.imageURI} 
+                  alt={`Character ${characterNFT.name}`} 
+                  />
+                <div className='health-bar'>
+                  <progress value={characterNFT.hp} max={characterNFT.maxHp}/>
+                  <p>{`${characterNFT.hp} / ${characterNFT.maxHp} HP `}</p>
+                </div>
+              </div>
+              <div className='stats'>
+                <h4>{`Attack Damage: ${characterNFT.attackDamage}`}</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
 
-      <p>Character</p>
+      }
     </div>
   )
  }
