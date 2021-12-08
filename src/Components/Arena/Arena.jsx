6f1b4bfa0bf82453292f75a9eaacee8a2c0d2720
Andrew McLean <88289750+itsreallydrew@users.noreply.github.com>
2021-12-08 00:38:03 +0000

    code to fetch boss completed

--- src/Components/Arena/Arena.jsx
+++ src/Components/Arena/Arena.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import {ethers} from 'ethers';
-import {CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import {CONTRACT_ADDRESS, transformCharacterData} from '../../constants';
 import myDopeGame from '../../utils/MyDopeGame.json';
 import './Arena.css';
 
 const Arena = ({characterNFT}) => {
   const [gameContract, setGameContract] = useState(null)
+  const [boss, setBoss] = useState(null)
 
   useEffect(() => {
     const {ethereum} = window;
@@ -13,13 +14,25 @@ const Arena = ({characterNFT}) => {
     if (ethereum) {
       const provider = new ethers.providers.Web3Provider(ethereum)
       const signer = provider.getSigner()
-      const gameContract = new ethers.Contract(provider, myDopeGame.abi, signer)
+      const gameContract = new ethers.Contract(CONTRACT_ADDRESS, myDopeGame.abi, signer)
 
       setGameContract(gameContract)
     } else {
       console.log('Ethereum object not found')
     }
   }, [])
+
+  useEffect(() => {
+    const fetchBoss = async () => {
+      const bossTxn = await gameContract.getBigBoss()
+      console.log(bossTxn)
+      setBoss(transformCharacterData(bossTxn))
+    }
+
+    if (gameContract) {
+      fetchBoss()
+    }
+  }, [gameContract])
 
   return (
     <div className='arena-container'>
